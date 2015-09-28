03b86da613f72fcd2dca32e3b39bd5bbea7637c5
Guangyi Wang <guangyiwang@gmail.com>
2015-09-27 23:31:23 -0700

    remove punctuation

--- src/main/java/com/gwang/jing/service/TextAnalysisService.java
+++ src/main/java/com/gwang/jing/service/TextAnalysisService.java
@@ -1,14 +1,33 @@
 package com.gwang.jing.service;
 import org.springframework.stereotype.Service;
+
+import com.google.common.collect.Sets;
+
 import java.util.List;
+import java.util.Set;
 import java.util.LinkedList;
 @Service
 public class TextAnalysisService {
-	public List<String> tokennize(String text) {
+	private static Set<Character> punctuationSet = Sets.newHashSet(',');
+	
+    public List<String> tokennize(String text) {
 	    List<String> result = new LinkedList<String> ();
 		for (int i = 0;i < text.length();i++) {
-		    result.add(String.valueOf(text.charAt(i)));
+			Character c = text.charAt(i);
+			if (!isPunctuation(c)) {
+		        result.add(String.valueOf(c));
+			}
 	    }
 		return result;
+	}
+	
+	public boolean isPunctuation(Character c) {
+		if (Character.isSpaceChar(c)) {
+			return true;
+		} else if (punctuationSet.contains(c)){
+			return true;
+		}
+		
+		return false;
 	}
 }
